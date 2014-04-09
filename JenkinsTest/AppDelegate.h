7721e4a0135056528a700cd9e3dfc2eeaf5572d2
Mark Struzinski <mstruzinski@paypal.com>
2014-04-08 22:25:21 -0400

    Initial Commit

---
+++ JenkinsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JenkinsTest
+//
+//  Created by Mark Struzinski on 4/8/14.
+//  Copyright (c) 2014 Apptronomy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JenkinsTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JenkinsTest
+//
+//  Created by Mark Struzinski on 4/8/14.
+//  Copyright (c) 2014 Apptronomy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
