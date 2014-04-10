6225b401100fa64dfd6be93037c35797498e84f0
Mark Struzinski <ski081@gmail.com>
2014-04-10 07:15:18 -0400

    Added pods and workspace.

--- JenkinsTestTests/JenkinsTestTests.m
+++ JenkinsTestTests/JenkinsTestTests.m
@@ -1,34 +1,12 @@
-//
-//  JenkinsTestTests.m
-//  JenkinsTestTests
-//
-//  Created by Mark Struzinski on 4/8/14.
-//  Copyright (c) 2014 Apptronomy. All rights reserved.
-//
+#import "Kiwi.h"
 
-#import <XCTest/XCTest.h>
+SPEC_BEGIN(Test)
 
-@interface JenkinsTestTests : XCTestCase
+describe(@"Math", ^{
+   it(@"is hard", ^{
+       int result = 4 + 2;
+       [[theValue(result) should] equal:theValue(6)];
+   });
+});
 
-@end
-
-@implementation JenkinsTestTests
-
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
-
-@end
+SPEC_END
